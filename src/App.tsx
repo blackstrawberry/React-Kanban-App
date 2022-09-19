fe525fc4701bf252c2008d0e1b824afe84a42ab3
김종한 <jong@BlackStrawberry.local>
2022-09-19 21:31:55 +0900

    #6.1 Selections done.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilState} from "recoil";
 import { hourSelector, minuteState } from "./atoms";
 
 function App() {
   const [minutes, setMinutes] = useRecoilState(minuteState);
-  const hours = useRecoilValue(hourSelector);
+  const [hours, setHours] = useRecoilState(hourSelector);
   const onMinuteChange = (event:React.FormEvent<HTMLInputElement>) => {
     setMinutes(+event.currentTarget.value);
   };
+  
+  const onHoursChange = (event:React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
+  }
   return (
     <>
       <input type="number" placeholder="Minutes" onChange={onMinuteChange} value={minutes}/>
-      <input type="number" placeholder="Hours" value={hours}/>
+      <input type="number" placeholder="Hours" onChange={onHoursChange} value={hours}/>
     </>
   );
 }
